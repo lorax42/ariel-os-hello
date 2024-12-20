e2005ee29aab34b960cad69da5b400281ce9acca
Kaspar Schleiser <kaspar@schleiser.de>
2024-12-20 10:43:45 +0100

    build: more ariel-os updates

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 #![feature(type_alias_impl_trait)]
 #![feature(used_with_arg)]
 
-use ariel_os::debug::{exit, log::info};
+use ariel_os::debug::{exit, log::info, ExitCode};
 
 #[ariel_os::thread(autostart)]
 fn main() {
@@ -11,5 +11,5 @@ fn main() {
         "Hello from main()! Running on a {} board.",
         ariel_os::buildinfo::BOARD
     );
-    exit(Ok(()));
+    exit(ExitCode::SUCCESS);
 }
